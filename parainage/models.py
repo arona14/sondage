b102979dfee2ad76e2714b7894be07494e72a242
arona91 <arona@ctsfares.com>
2018-05-16 16:49:31 +0000

    new model person

--- parainage/models.py
+++ parainage/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class BaseEntity(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Person(BaseEntity):
+    name = models.CharField(max_length=50)
