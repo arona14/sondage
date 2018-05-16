047893c3252089c14bedc910c3575868f9569303
arona91 <arona@ctsfares.com>
2018-05-16 16:57:42 +0000

    Comment entity de base

--- parainage/models.py
+++ parainage/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class BaseEntity(models.Model):
+class BaseEntity(models.Model): # Entity de base
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
